72dde289a804403569c0f088e7a4d34d8d7ce447
skmasumdeveloper <masum4a@gmail.com>
2021-01-07 18:51:28 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -37,155 +38,174 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController name = new TextEditingController();
   TextEditingController password = new TextEditingController();
   bool _showPassword = false;
+  var formmsgsuccess = '';
+  var formmsgerror = '';
+  var resmsg;
 
   Future<void> senddata() async {
-    final response =
-        await http.post("http://192.168.29.50/flutterform/register.php", body: {
+    final response = await http
+        .post("http://192.168.31.103/flutterform/register.php", body: {
       'username': name.text,
       'password': password.text,
     });
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+    resmsg = json.decode(response.body);
 
     setState(() {
       name.clear();
       password.clear();
+      formmsgsuccess = resmsg['msg2'];
+      formmsgerror = resmsg['msg1'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        flexibleSpace: Container(
-          decoration: new BoxDecoration(
-            gradient: new LinearGradient(
-                colors: [
-                  const Color(0xFF3366FF),
-                  const Color(0xFF00CCFF),
-                ],
-                begin: const FractionalOffset(0.0, 0.0),
-                end: const FractionalOffset(1.0, 0.0),
-                stops: [0.0, 1.0],
-                tileMode: TileMode.clamp),
-          ),
-        ),
+    return Container(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+            colors: [
+              Colors.deepPurple[700],
+              Colors.purple[500],
+            ],
+            begin: const FractionalOffset(0.5, 0.0),
+            end: const FractionalOffset(0.0, 0.5),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
       ),
-      drawer: Drawer(),
-      body: Center(
-        child: Container(
-          decoration: new BoxDecoration(
-            gradient: new LinearGradient(
-                colors: [
-                  Colors.deepPurple[700],
-                  Colors.purple[500],
-                ],
-                begin: const FractionalOffset(0.5, 0.0),
-                end: const FractionalOffset(0.0, 0.5),
-                stops: [0.0, 1.0],
-                tileMode: TileMode.clamp),
-          ),
-          padding: EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Username",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(fontSize: 17.0),
-                  ),
-                  TextField(
-                    controller: name,
-                    autofocus: true,
-                    style: TextStyle(color: Colors.white, fontSize: 23),
-                    decoration: InputDecoration(
-                      hintText: 'Enter Your Name',
-                      hintStyle: TextStyle(color: Colors.grey),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        drawer: Drawer(),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Username",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 17.0),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                    controller: password,
-                    obscureText: !_showPassword,
-                    autofocus: true,
-                    style: TextStyle(color: Colors.white, fontSize: 23),
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: 'Enter Your Password',
-                      labelStyle: TextStyle(color: Colors.white),
-                      hintStyle: TextStyle(color: Colors.grey),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
+                    TextField(
+                      controller: name,
+                      //autofocus: true,
+                      style: TextStyle(color: Colors.white, fontSize: 23),
+                      decoration: InputDecoration(
+                        hintText: 'Enter Your Name',
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      controller: password,
+                      obscureText: !_showPassword,
+                      //autofocus: true,
+                      style: TextStyle(color: Colors.white, fontSize: 23),
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: 'Enter Your Password',
+                        labelStyle: TextStyle(color: Colors.white),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _showPassword = !_showPassword;
+                            });
+                          },
+                          child: Icon(
+                            _showPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                BorderSide(width: 2, color: Colors.green)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                BorderSide(width: 2, color: Colors.green)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: formmsgerror == "fail"
+                          ? Text(
+                              formmsgsuccess,
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 20,
+                              ),
+                            )
+                          : Text(
+                              formmsgsuccess,
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 20,
+                              ),
+                            ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 40,
+                      child: RaisedButton(
+                        color: Colors.green,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text("Register"),
+                        onPressed: () {
+                          senddata();
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 200,
+                      child: RaisedButton(
+                        color: Colors.black,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text("Clear"),
+                        onPressed: () {
                           setState(() {
-                            _showPassword = !_showPassword;
+                            name.clear();
+                            password.clear();
                           });
                         },
-                        child: Icon(
-                          _showPassword
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                        ),
                       ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide:
-                              BorderSide(width: 2, color: Colors.green)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide:
-                              BorderSide(width: 2, color: Colors.green)),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 200,
-                    height: 40,
-                    child: RaisedButton(
-                      color: Colors.green,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text("Register"),
-                      onPressed: () {
-                        senddata();
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    width: 200,
-                    child: RaisedButton(
-                      color: Colors.black,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text("Clear"),
-                      onPressed: () {
-                        setState(() {
-                          name.clear();
-                          password.clear();
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
